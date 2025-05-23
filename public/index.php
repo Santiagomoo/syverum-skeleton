d8446adf9b0ab3df8b15ea5d03c51ca900c2ad51
Santiago M <shago2304@gmail.com>
2025-05-23 13:35:28 -0500

    Define BASE_PATH constant to set the project root directory

--- public/index.php
+++ public/index.php
@@ -2,10 +2,11 @@
 
 use Core\Boot\Application;
 
-require __DIR__ . '/../vendor/autoload.php';
+define('BASE_PATH', dirname(__DIR__, 1));
+
+
+require BASE_PATH . '/vendor/autoload.php';
 
 
 $app = new Application();
 $app->run();
-
-
