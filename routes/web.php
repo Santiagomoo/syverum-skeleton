e0f84949bd277ede563839bee4fad91bc46406f2
Santiagomoo <shago2304@gmail.com>
2025-08-13 12:44:20 -0500

    Add base middleware file

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Core\Facades\Route;
 use App\Http\Controllers\HomeController;
 
 
-Route::get('/',[HomeController::class,'index'])->name('index');
+Route::get('/',[HomeController::class,'index'])->name('index')->middleware('ExampleMiddleware');
 Route::get('/servicios',[HomeController::class,'service'])->name('service');
 Route::get('/acercade',[HomeController::class,'about'])->name('about');
 
